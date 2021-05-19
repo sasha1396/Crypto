ff2aed837e7c2ba08e4a4cbeb0d9c5c79e0bc6f6
Sasha <nikotin@nxt.ru>
2021-05-20 00:13:55 +0300

    update main.go
    Added separately print hash value in other function and shut down program with Ctrl+C

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
 	"runtime"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -20,41 +23,73 @@ func main() {
 
 	gogroup.Add(runtime.NumCPU()) // группа из 6 горутин
 
+	hash := make(chan [32]byte)
+	tm := make(chan time.Duration)
+	c := make(chan int)
+
 	//запускаю горутины (количество ядер = количеству горутин)
 	for i := 0; i < runtime.NumCPU(); i++ {
-		go findhash(i)
+		go findhash(i, c, interrupt(), hash, tm)
 	}
+	//горутина для вывода хеша
+	go printhash(hash, c, tm)
 
 	gogroup.Wait() // жду завершения горутин
+	fmt.Println("Программа завершена")
 }
 
-func findhash(id int) {
+func findhash(id int, c chan int, quit <-chan os.Signal, hash chan [32]byte, tm chan time.Duration) {
 	defer gogroup.Done()
 
 	start := time.Now()
 	tflag := true
-
 	buf := make([]byte, binary.MaxVarintLen64) // срез для байтов случайного числа
 	number := rand.Int63()                     // случайное число
 
 	for {
-		// метка отсчета выполнения поиска
-		if tflag {
-			start = time.Now()
-			tflag = false
-		}
-		n := binary.PutVarint(buf, number) // количество байтов
-		sum := sha256.Sum256(buf)          // хеш от числа
-		hex := fmt.Sprintf("%x", sum[:])
-		number++
+		select {
+		case <-quit:
+			fmt.Println("Поток:", id, "завершил работу")
+			return
+		default:
+			// метка отсчета выполнения поиска
+			if tflag {
+				start = time.Now()
+				tflag = false
+			}
+			binary.PutVarint(buf, number) // количество байтов
+			sum := sha256.Sum256(buf)     // хеш от числа
+			hex := fmt.Sprintf("%x", sum[:])
+			number++
 
-		if hex[:len(prefix)] == prefix {
-			fmt.Printf("хеш: %x\n", sum)
-			fmt.Printf("%x\n", buf[:n])
-			t := time.Now()         // метка конца поиска
-			elapsed := t.Sub(start) // вычисления разницы между меткой начала и конца
-			fmt.Println("Поток:", id, "Время нахождения:", elapsed)
-			tflag = true // возвращаем флаг чтобы снова отсчитывать время
+			if hex[:len(prefix)] == prefix {
+				hash <- sum
+				t := time.Now()         // метка конца поиска
+				elapsed := t.Sub(start) // вычисления разницы между меткой начала и конца
+				tm <- elapsed
+				c <- id
+				tflag = true // возвращаем флаг чтобы снова отсчитывать время
+			}
+
 		}
 	}
+}
+
+// вывод хэша
+func printhash(hash chan [32]byte, c chan int, tm chan time.Duration) {
+	for {
+		fmt.Printf("Хеш: %x\n", <-hash)
+		fmt.Println("Время нахождения:", <-tm, "Поток:", <-c)
+	}
+}
+
+// вызов прерывания
+func interrupt() <-chan os.Signal {
+	c := make(chan os.Signal, 1)
+	signal.Notify(
+		c,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	return c
 }
